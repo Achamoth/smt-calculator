3701a6d868ff3fb1f71d908a0eae9d0e443690b5
Ammar.AbuShamleh <ammar.abushamleh@health.telstra.com>
2021-11-12 18:00:03 +0800

    Getting started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { Demon } from "./Demon";
+import data from "./data.json";
 
-function App() {
+function TestList() {
+  console.log(data);
+  let demons = data.map(
+    (d) =>
+      new Demon(d.race, d.name, d.level, d.skills, d.resistances, d.affinities)
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="TestList">
+      <table className="table">
+        <tbody>
+          {demons.map((x) => {
+            return (
+              <tr className="tableRow" key={x.name}>
+                <td>{x.race}</td>
+                <td>{x.name}</td>
+                <td>{x.level}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
 
-export default App;
+export default TestList;
