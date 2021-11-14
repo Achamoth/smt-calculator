4a90fb964ef53704ca16d185e7485dcc9977d2da
Achamoth <21521274@student.uwa.edu.au>
2021-11-14 15:55:06 +0800

    Finished normal demon fusion calcs

--- src/utils/demon_fusion.js
+++ src/utils/demon_fusion.js
@@ -2,13 +2,18 @@ import { SMT5_FUSION_CHART } from "../smt_v_data/fusion_chart.js";
 import { SMT5_ELEMENT_CHART } from "../smt_v_data/element_chart.js";
 import { parse_demons } from "./demon_utils.js";
 
+const ElementTransforms = {
+  UP: 1,
+  DOWN: -1,
+};
+
+// Get all immediate fusion combinations for demon, disregarding skills
 export function getFusionCombinations(demon) {
   let combinations = [];
   let demons = parse_demons();
 
   // Special fusion
 
-  // Element
   if (demon.race === "Element") {
     let racesToProduce = findRacesThatProduceElement(demon.name);
     for (const race of racesToProduce) {
@@ -19,11 +24,24 @@ export function getFusionCombinations(demon) {
     return combinations.flat();
   }
 
-  // Elements up
+  let elementsUp = getElementsWithDesiredTransformForRace(
+    demons,
+    demon.race,
+    ElementTransforms.UP
+  );
+  combinations.push(
+    findCombinationsForElementAscension(demons, elementsUp, demon)
+  );
 
-  // Elements down
+  let elementsDown = getElementsWithDesiredTransformForRace(
+    demons,
+    demon.race,
+    ElementTransforms.DOWN
+  );
+  combinations.push(
+    findCombinationsForElementDescension(demons, elementsDown, demon)
+  );
 
-  // Normal fusions
   let racePairs = getRacePairsForDesiredRace(demon.race);
   racePairs.forEach((r, _) => {
     combinations.push(getCombinationsFromRaces(demon, demons, r[0], r[1]));
@@ -73,6 +91,63 @@ function getCombinationsFromRaces(desiredDemon, demonList, race1, race2) {
         result.push([d1, d2]);
       }
     });
+  });
+  return result;
+}
+
+function getElementsWithDesiredTransformForRace(
+  demonList,
+  race,
+  desiredTransform
+) {
+  let result = [];
+  let elementTransforms =
+    SMT5_ELEMENT_CHART.table[
+      SMT5_ELEMENT_CHART.races.findIndex((r) => r === race)
+    ];
+  elementTransforms.forEach((t, i) => {
+    if (t === desiredTransform) {
+      result.push(
+        demonList.find((d) => d.name === SMT5_ELEMENT_CHART.elems[i])
+      );
+    }
+  });
+  return result;
+}
+
+function findCombinationsForElementAscension(demons, elementsUp, demon) {
+  let result = [];
+  let demonsInRace = demons
+    .filter((d) => d.race === demon.race)
+    .sort((d1, d2) => (d1.level < d2.level ? -1 : 1));
+
+  if (demon.name === demonsInRace[0].name) {
+    elementsUp.forEach((e) => {
+      result.push([e, demonsInRace[demonsInRace.length - 1]]);
+    });
+    return result;
+  }
+
+  let targetDemonIndex = demonsInRace.findIndex((d) => d.name === demon.name);
+  elementsUp.forEach((e) => {
+    result.push([e, demonsInRace[targetDemonIndex - 1]]);
+  });
+  return result;
+}
+
+function findCombinationsForElementDescension(demons, elementsDown, demon) {
+  let result = [];
+  let demonsInRace = demons
+    .filter((d) => d.race === demon.race)
+    .sort((d1, d2) => (d1.level < d2.level ? -1 : 1));
+
+  if (demon.name === demonsInRace[demonsInRace.length - 1].name) {
+    return result;
+  }
+
+  let targetDemonIndex = demonsInRace.findIndex((d) => d.name === demon.name);
+  elementsDown.forEach((e) => {
+    result.push([e, demonsInRace[targetDemonIndex + 1]]);
   });
   return result;
 }
