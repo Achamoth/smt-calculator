01bb511237996f384ac8995af05fb5f792043a38
Achamoth <21521274@student.uwa.edu.au>
2021-11-14 19:03:42 +0800

    NavBar on demon  page

--- src/DemonFusions.js
+++ src/DemonFusions.js
@@ -1,6 +1,7 @@
 import { useParams, Link } from "react-router-dom";
 import "./DemonFusions.css";
 import { getFusionCombinations } from "./utils/demon_fusion.js";
+import { NavBar } from "./NavBar.js";
 
 export function DemonFusions(props) {
   let name = useParams().demonName;
@@ -10,35 +11,38 @@ export function DemonFusions(props) {
   return (
     <div>
       <div className="centeredContainer">
-        <div className="demonFusionContents">
-          <div className="demon">
-            <h1>{`LV${demon.level} ${demon.race} ${demon.name}`}</h1>
-            <table className="demonSkills">
-              <thead>
-                <tr>
-                  <th className="demonSkillsHeader">Skill</th>
-                  <th className="demonSkillsHeader">Level</th>
-                </tr>
-              </thead>
-              <tbody>
-                {demon.skills
-                  .sort((s1, s2) => (s1.level < s2.level ? -1 : 1))
-                  .map((s) => {
-                    return (
-                      <tr className="demonSkillRow" key={s.name}>
-                        <td className="demonSkillCell">{s.name}</td>
-                        <td className="demonSkillCell">
-                          {s.level === 0
-                            ? "Innate"
-                            : s.level === 5277
-                            ? "Tm"
-                            : s.level}
-                        </td>
-                      </tr>
-                    );
-                  })}
-              </tbody>
-            </table>
+        <div display="block">
+          <NavBar />
+          <div className="demonFusionContents">
+            <div className="demon">
+              <h1>{`LV${demon.level} ${demon.race} ${demon.name}`}</h1>
+              <table className="demonSkills">
+                <thead>
+                  <tr>
+                    <th className="demonSkillsHeader">Skill</th>
+                    <th className="demonSkillsHeader">Level</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {demon.skills
+                    .sort((s1, s2) => (s1.level < s2.level ? -1 : 1))
+                    .map((s) => {
+                      return (
+                        <tr className="demonSkillRow" key={s.name}>
+                          <td className="demonSkillCell">{s.name}</td>
+                          <td className="demonSkillCell">
+                            {s.level === 0
+                              ? "Innate"
+                              : s.level === 5277
+                              ? "Tm"
+                              : s.level}
+                          </td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
