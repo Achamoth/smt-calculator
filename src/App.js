443696635d88dc6ebb05523424acac6058645391
Ammar Abu Shamleh <ammar.abushamleh@health.telstra.com>
2021-11-15 15:04:46 +0800

    Update router using basename

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import { parse_demons } from "./utils/demon_utils.js";
 import { DemonTable } from "./DemonTable.js";
 import { DemonFusions } from "./DemonFusions";
 import { Skills } from "./Skills.js";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { FusionRecipe } from "./FusionRecipe.js";
 
 function App() {
   let demons = parse_demons();
 
   return (
-    <Router>
+    <Router basename={window.location.pathname || ""}>
       <Routes>
         <Route path="/" element={<DemonTable demons={demons} />} />
         <Route path="/skills" element={<Skills />} />
